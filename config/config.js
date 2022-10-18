4e25637af50f0d62ab03220d629cb0d3e834d62c
Alex Damovski <adamovsk@my.centennialcollege.ca>
2022-10-17 20:16:36 -0400

    Week6 Commit

---
+++ config/config.js
@@ -0,0 +1,3 @@
+export const Secret = "MySecret";
+// export const MongoURI  = "mongodb://127.0.0.1/media"
+export const MongoURI  = "mongodb+srv://ADamovski:zv5qnykzf210oZ1L@comp229.kofpvto.mongodb.net/?retryWrites=true&w=majority" 
