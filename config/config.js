879212b70bc07d8abef0757536773065eaeccaf2
AlexDamovski <112990760+AlexDamovski@users.noreply.github.com>
2022-11-07 21:38:22 -0500

    test

--- config/config.js
+++ config/config.js
@@ -1,3 +1,3 @@
 export const Secret = "MySecret";
 // export const MongoURI  = "mongodb://127.0.0.1/media"
-export const MongoURI  = "mongodb+srv://ADamovski:CRaLOPUKok4ah7GQ@cluster0.fxpwhwv.mongodb.net/?retryWrites=true&w=majority" 
+export const MongoURI  = "mongodb+srv://XTrak:ByPBTVGTTWipqDEQ@cluster0.fxpwhwv.mongodb.net/?retryWrites=true&w=majority" 
